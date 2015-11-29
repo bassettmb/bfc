71c405aabf27f801c65bdfe1f8d574f8f3bb3fb6
Michael Bassett <michael.b.bassett@gmail.com>
2015-11-29 16:46:57 -0600

    Re-export concrete ast::* types to maintain existing interface.

--- ast.hpp
+++ ast.hpp
@@ -6,4 +6,21 @@
 #include "ast/mod.hpp"
 #include "ast/visitor.hpp"
 
+namespace bfc {
+
+using ast_node = ast::node;
+
+using ast_program = ast::program;
+using ast_loop = ast::loop;
+
+using ast_mov = ast::mov;
+using ast_add = ast::add;
+using ast_sub = ast::sub;
+using ast_mul = ast::mul;
+
+using ast_read = ast::read;
+using ast_write = ast::write;
+
+}
+
 #endif /* !BFC_AST_HPP */
