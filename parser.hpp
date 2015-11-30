da961cafc831c89d09e676988ae4ff2b5de7e82e
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-11-30 22:10:27 +0000

    Change pos to loc in parser

--- parser.hpp
+++ parser.hpp
@@ -41,7 +41,7 @@ class Parser {
         /* vector of instructions to return */
         std::vector<ast_node> ast;
 
-        ast_loop updateLoop(position loopPos){
+        ast_loop updateLoop(source_loc loopPos){
             /* create result to hold lexer result */
             result_type res = result_type::OK;
             /* create token to pass into lexer */
@@ -49,44 +49,44 @@ class Parser {
             /* create vector to use to create ast_loop */
             std::vector<ast_node> loopAst;
             token::type kind;
-            position pos;
+            source_loc loc;
 
             for(;;) {
                 res = lexer.next(tok);
                 switch (res) {
                     case result_type::OK:
                         kind = tok.kind;
-                        pos = tok.pos;
+                        loc = tok.loc;
                         switch (kind) {
                             case token::INC:
-                                ast_add add(pos, 0, 1);
+                                ast_add add(loc, 0, 1);
                                 loopAst.push_back(add);
                                 break;
                             case token::DEC:
-                                ast_sub sub(pos, 0, 1);
+                                ast_sub sub(loc, 0, 1);
                                 loopAst.push_back(sub);
                                 break;
                             case token::MOVE_R:
-                                ast_mov move_r(pos, 1);
+                                ast_mov move_r(loc, 1);
                                 loopAst.push_back(move_r);
                                 break;
                             case token::MOVE_L:
-                                ast_mov move_l(pos, -1);
+                                ast_mov move_l(loc, -1);
                                 loopAst.push_back(move_l);
                                 break;
                             case token::LOOP_BEGIN:
-                                ast_loop innerLoop = updateLoop(pos);
+                                ast_loop innerLoop = updateLoop(loc);
                                 loopAst.push_back(innerLoop);
                                 break;
                             case token::LOOP_END:
                                 ast_loop loop(loopPos, loopAst.begin(), loopAst.end());
                                 return loop;
                             case token::PUT_CHAR:
-                                ast_write write(pos, 0);
+                                ast_write write(loc, 0);
                                 loopAst.push_back(write);
                                 break;
                             case token::GET_CHAR:
-                                ast_read read(pos, 0);
+                                ast_read read(loc, 0);
                                 loopAst.push_back(read);
                                 break;
                             default:
@@ -102,37 +102,37 @@ class Parser {
 
         void updateAst(token &tok) {
             token::type kind = tok.kind;
-            position pos = tok.pos;
+            source_loc loc = tok.loc;
 
             switch (kind) {
                 case token::INC:
-                    ast_add add(pos, 0, 1);
+                    ast_add add(loc, 0, 1);
                     ast.push_back(add);
                     break;
                 case token::DEC:
-                    ast_sub sub(pos, 0, 1);
+                    ast_sub sub(loc, 0, 1);
                     ast.push_back(sub);
                     break;
                 case token::MOVE_R:
-                    ast_mov move_r(pos, 1);
+                    ast_mov move_r(loc, 1);
                     ast.push_back(move_r);
                     break;
                 case token::MOVE_L:
-                    ast_mov move_l(pos, -1);
+                    ast_mov move_l(loc, -1);
                     ast.push_back(move_l);
                     break;
                 case token::LOOP_BEGIN:
-                    ast_loop loop = updateLoop(pos);
+                    ast_loop loop = updateLoop(loc);
                     ast.push_back(loop);
                     break;
                 case token::LOOP_END:
                     // Throw Exception (No open loop to close)
                 case token::PUT_CHAR:
-                    ast_write write(pos, 0);
+                    ast_write write(loc, 0);
                     ast.push_back(write);
                     break;
                 case token::GET_CHAR:
-                    ast_read read(pos, 0);
+                    ast_read read(loc, 0);
                     ast.push_back(read);
                     break;
                 default:
