3f4b047ea88c386df4b039fe849d20fcf0ca8062
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-11-30 03:04:54 +0000

    Basic sort of recursive descent parser

--- parser.hpp
+++ parser.hpp
@@ -14,7 +14,7 @@ class Parser {
 
         Parser(Lexer lexer) : this.lexer(lexer) {}
 
-        std:vector<AstNode> parse() {
+        ast_program parse() {
             /* create result to hold lexer result */
             result_type res = result_type::OK;
             /* create token to pass into lexer */
@@ -39,54 +39,101 @@ class Parser {
         Lexer lexer;
 
         /* vector of instructions to return */
-        std::vector<std::unique_ptr<AstNode>> ast;
+        std::vector<ast_node> ast;
 
-        /* stack to manage nested instructions in loop nodes */
-        std::stack<std::vector<std::unique_ptr<AstNode>>> stack;
+        ast_loop updateLoop(position loopPos){
+            /* create result to hold lexer result */
+            result_type res = result_type::OK;
+            /* create token to pass into lexer */
+            token tok;
+            /* create vector to use to create ast_loop */
+            std::vector<ast_node> loopAst;
+            token::type kind;
+            position pos;
+
+            for(;;) {
+                res = lexer.next(tok);
+                switch (res) {
+                    case result_type::OK:
+                        kind = tok.kind;
+                        pos = tok.pos;
+                        switch (kind) {
+                            case token::INC:
+                                ast_add add(pos, 0, 1);
+                                loopAst.push_back(add);
+                                break;
+                            case token::DEC:
+                                ast_sub sub(pos, 0, 1);
+                                loopAst.push_back(sub);
+                                break;
+                            case token::MOVE_R:
+                                ast_mov move_r(pos, 1);
+                                loopAst.push_back(move_r);
+                                break;
+                            case token::MOVE_L:
+                                ast_mov move_l(pos, -1);
+                                loopAst.push_back(move_l);
+                                break;
+                            case token::LOOP_BEGIN:
+                                ast_loop innerLoop = updateLoop(pos);
+                                loopAst.push_back(innerLoop);
+                                break;
+                            case token::LOOP_END:
+                                ast_loop loop(loopPos, loopAst.begin(), loopAst.end());
+                                return loop;
+                            case token::PUT_CHAR:
+                                ast_write write(pos, 0);
+                                loopAst.push_back(write);
+                                break;
+                            case token::GET_CHAR:
+                                ast_read read(pos, 0);
+                                loopAst.push_back(read);
+                                break;
+                            default:
+                        }
+                        break; /* result_type::OK */
+                    case result_type::DONE:
+                    case result_type::FAIL:
+                    default:
+                        // Throw Exception (ends without closing loop)
+                }
+            }
+        }
 
         void updateAst(token &tok) {
             token::type kind = tok.kind;
             position pos = tok.pos;
-            std::unique_ptr<AstNode> node;
 
             switch (kind) {
                 case token::INC:
-                    node = std::make_unique<AddNode>(pos, 1);
-                    ast.push_back(node);
+                    ast_add add(pos, 0, 1);
+                    ast.push_back(add);
                     break;
                 case token::DEC:
-                    node = std::make_unique<AddNode>(pos, -1);
-                    ast.push_back(node);
+                    ast_sub sub(pos, 0, 1);
+                    ast.push_back(sub);
                     break;
                 case token::MOVE_R:
-                    node = std::make_unique<MoveNode>(pos, 1);
-                    ast.push_back(node);
+                    ast_mov move_r(pos, 1);
+                    ast.push_back(move_r);
                     break;
                 case token::MOVE_L:
-                    node = std::make_unique<MoveNode>(pos, -1);
-                    ast.push_back(node);
+                    ast_mov move_l(pos, -1);
+                    ast.push_back(move_l);
                     break;
                 case token::LOOP_BEGIN:
-                    stack.push(ast);
-                    ast = std::vector<std::unique_ptr<AstNode>>();
+                    ast_loop loop = updateLoop(pos);
+                    ast.push_back(loop);
                     break;
                 case token::LOOP_END:
-                    if (!stack.empty()) {
-                        node = std::make_unique<LoopNode>(pos, ast);
-                        ast = stack.top();
-                        stack.pop();
-                        ast.push_back(node);
-                    } else {
-                        // throw exception
-                    }
-                    break;
+                    // Throw Exception (No open loop to close)
                 case token::PUT_CHAR:
-                    node = std::make_unique<WriteNode>(pos);
-                    ast.push_back(node);
+                    ast_write write(pos, 0);
+                    ast.push_back(write);
                     break;
                 case token::GET_CHAR:
-                    node = std::make_unique<ReadNode>(pos);
-                    ast.push_back(node);
+                    ast_read read(pos, 0);
+                    ast.push_back(read);
                     break;
                 default:
 
