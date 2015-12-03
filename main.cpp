a06792ec71ed9a8b29c1e111ae949d1add0b3250
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-12-03 01:06:27 +0000

    Fix templating of parser and update main up through parsing

--- main.cpp
+++ main.cpp
@@ -1,52 +1,59 @@
 #include <cstdio>
 #include <cstring>
+#include <fstream>
 
 static const char PROGNAME[] = "bfc";
+static const char FILETYPE[] = "bf";
+static const size_t FILETYPE_LEN = 2;
 // TODO: add version number (requires configure setup)
+
+using namespace bfc;
 
 static void
 print_usage(FILE *out)
 {
-  fprintf(out, "usage: %s <source-file> [<source-file>]", PROGNAME);
+  fprintf(out, "usage: %s <source-file>", PROGNAME);
 }
 
-static int 
-handle_filepath(const char *filepath);
+static void
+print_filename_error(FILE *out)
+{
+  fprintf(out, "file must be of type %s", FILETYPE);
+}
+
+static int
+handle_filepath(const char *filepath)
+{
+  size_t len = strlen(filepath);
+  if (filepath[len - FILETYPE_LEN - 1] != '.'){
+    print_filename_error(stderr);
+    return -1;
+  }
+  for (size_t i = FILETYPE_LEN ; i > 0; --i){
+    if (filepath[len - i] != FILETYPE[FILETYPE_LEN - i]){
+      print_filename_error(stderr);
+      return -1;
+    }
+  }
+
+  std::fstream * stream = new std::fstream(filepath);
+  stream_source streamSource(stream);
+  lexer<stream_source> lexer(streamSource);
+  parser<stream_source> parser(lexer);
+  ast_program ast = parser.parse();
+
+  return 0;
+}
 
 int
 main(int argc, const char *const *argv)
 {
-  if (argc < 2) {
+  if (argc != 2) {
     print_usage(stderr);
     exit(EXIT_FAILURE);
   }
 
-  for (const char *off = argv; *off; off += 1) {
-
-    int ch;
-    const char *arg = off;
-
-    ch = arg[0];
-    if (ch != '-')
-      continue;
-
-    /* handle -h */
-    ch = arg[1];
-    if (ch == 'h') {
-      print_usage(stdout);
-      exit(EXIT_SUCCESS);
-    }
-
-    /* handle --help*/
-    if (ch == '-' && !strcmp(arg + 2, "help")) {
-      print_usage(stdout);
-      exit(EXIT_SUCCESS);
-    }
-
-  }
-
-  for (const char *off = argv; *off; off += 1)
-    handle_filepath(off);
+  handle_filepath(argv[1]);
 
   return 0;
 }
