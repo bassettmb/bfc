f0e4cbf8b15a5791c2244806c5cf6b571c416bc6
Michael Bassett <michael.b.bassett@gmail.com>
2015-12-02 21:44:18 -0600

    make trait constructors protected to avoid accidental instantiation

--- ast/traits.hpp
+++ ast/traits.hpp
@@ -5,7 +5,7 @@
 #include "ast/base.hpp"
 #include "ast/seq.hpp"
 #include "ast/visitor.hpp"
-#include "source_loc.hpp"
+#include "sourceloc.hpp"
 #include <cstddef>
 
 namespace bfc {
@@ -36,21 +36,21 @@ private:
 /* Trait for nodes having a source position. */
 class has_loc {
 public:
-  has_loc(source_loc node_loc) noexcept : node_loc{std::move(node_loc)} {}
-  source_loc loc(void) const noexcept { return node_loc; }
+  sourceloc loc(void) const noexcept { return node_loc; }
 protected:
+  has_loc(sourceloc node_loc) noexcept : node_loc{std::move(node_loc)} {}
   ~has_loc() = default;
 private:
-  source_loc node_loc;
+  sourceloc node_loc;
 };
 
 /* Trait for nodes holding an offset (to the brainfuck heap pointer). */
 class has_offset {
 public:
-  has_offset(ptrdiff_t node_offset) noexcept : node_offset{node_offset} {}
   ptrdiff_t offset(void) const noexcept { return node_offset; }
   void offset(ptrdiff_t new_offset) noexcept { node_offset = new_offset; }
 protected:
+  has_offset(ptrdiff_t node_offset) noexcept : node_offset{node_offset} {}
   ~has_offset() = default;
 private:
   ptrdiff_t node_offset;
@@ -59,40 +59,39 @@ private:
 /* Trait for nodes holding a (brainfuck) value. */
 class has_value {
 public:
-  has_value(bf_value node_value) noexcept : node_value{node_value} {}
   bf_value value(void) const noexcept { return node_value; }
   void value(bf_value value) noexcept { node_value = value; }
+protected:
+  has_value(bf_value node_value) noexcept : node_value{node_value} {}
+  ~has_value() = default;
 private:
   bf_value node_value;
 };
 
 /* Abstract base class for arithmetic operation nodes. */
 class arith : public has_loc, public has_offset, public has_value {
-public:
-  arith(source_loc loc, ptrdiff_t offset, bf_value value) noexcept :
+protected:
+  arith(sourceloc loc, ptrdiff_t offset, bf_value value) noexcept :
     has_loc{loc}, has_offset{offset}, has_value{value}
   {}
-protected:
   ~arith() = default;
 };
 
 /* Abstract base class for pointer arithmetic operation nodes. */
 class ptr_op : public has_loc, public has_offset {
-public:
-  ptr_op(source_loc loc, ptrdiff_t offset) noexcept :
+protected:
+  ptr_op(sourceloc loc, ptrdiff_t offset) noexcept :
     has_loc{loc}, has_offset{offset}
   {}
-protected:
   ~ptr_op() = default;
 };
 
 /* Abstract base class for io operation nodes. */
 class io : public base, public has_loc, public has_offset {
-public:
-  io(source_loc loc, ptrdiff_t offset) noexcept :
+protected:
+  io(sourceloc loc, ptrdiff_t offset) noexcept :
     has_loc{std::move(loc)}, has_offset{offset}
   {}
-protected:
   ~io() = default;
 };
 
