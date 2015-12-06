83ff31da3aa4348976a9aeffcb4c1126257b4b5c
Connor <connor.r.mcgowan@vanderbilt.edu>
2015-12-06 16:49:15 -0600

    Updated optimizer entry point

--- optimizer.hpp
+++ optimizer.hpp
@@ -9,53 +9,46 @@ class optimizer {
     
     public:
     
-        optimizer(void) {}
+        optimizer(bool clean, bool quick) : clean(clean), quick(quick)  {}
         
-        ast_seq optimize(ast_seq & program) {
-            ast_base *prev = program.clone();
-            ast_seq res = optimize_pass(program);
-            while (*prev != res) {
-                prev = res.clone();
-                res = optimize_pass(res);
+        ast_node optimize(consts ast_node &program) {
+            // create list of optimizers
+            std::vector<ast_visitor> opts;
+            combine_inc_visitor inc;
+            opts.push_back(inc);
+            
+            if (clean) {
+                // add additional optimizers
+                dead_loops_visitor deadloop;
+                opts.push_back(deadloop);
             }
-            return res;
+            
+            opt_program_visitor opt_prog(std::move(opts));
+            // run optimizations
+            program.accept(opt_prog);
+            
+            if (!quick) {
+                if (!program.equals(opt_prog.result()) {
+                    // re-run optimizations until AST is stable
+                    ast_node result;
+                    do {
+                        result = opt_prog.result();
+                        result.accept(opt_prog);
+                    } while (result != opt_prog.result());
+                }
+            }
+            
+            return opt_prog.result();
+            
         }    
 
     private:
-    
-        ast_seq optimize_pass(ast_seq & ops) {
-            ast_seq res = combine_inc_mov(ops);
-            zero_loops_visitor(res);
-            multiply_loops_visitor(res);
-            
-        }
+        // indicates if redundant code should be cleaned up during optimization
+        bool clean;
         
-        ast_seq combine_inc_mov(ast_seq & ops) {
-            ast_seq res();
-            combine_inc_mov_visistor v(res);
-            status stat = ops.accept(v);
-            if (stat == BREAK) {
-                // error handling
-            } else {
-                return res;
-            }
-        }
-    
-};
-
-class combine_inc_mov_visitor : public ast_visitor {
-    
-    public:
-    
-        combine_inc_mov_visitor(ast_seq & seq) : res(seq)
-    
-        status visit(ast_add &node) {
-            
-        }
+        // indicates if one-pass or full optimization is performed
+        bool quick;
         
-    private:
-    
-        ast_seq res;
     
 };
 
