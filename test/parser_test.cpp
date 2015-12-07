28196fe418a8e5202975c321faa93c4fd69ebf5e
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-12-07 21:26:38 +0000

    Add file for parser test

---
+++ test/parser_test.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+#include "lexer.hpp"
+#include "parser.hpp"
+
+
+
+int main(int argc, char** argv)
+{
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
