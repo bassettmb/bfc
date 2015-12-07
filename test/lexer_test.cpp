d1cf1ecace544467ac1642fb790b86673982c46c
AJ Ballway <aj.ballway@vanderbilt.edu>
2015-12-07 21:16:45 +0000

    Lexer tests compile, pass

--- test/lexer_test.cpp
+++ test/lexer_test.cpp
@@ -1,4 +1,4 @@
-#include <fstream>
+#include <sstream>
 #include "gtest/gtest.h"
 #include "lexer.hpp"
 #include "result.hpp"
@@ -6,55 +6,53 @@
 #include "source_loc.hpp"
 #include "token.hpp"
 
-static const char EMPTY_FILE_NAME = "empty_file.bfc"
-static const char ONE_OF_EACH_FILE_NAME = "one_of_each.bfc"
 
 TEST(LexerTest, EmptyFile)
 {
-  bfc::lexer<bfc::stream_source> lexer{bfc::stream_source{new std::fstream{EMPTY_FILE_NAME}}};
+  bfc::lexer<bfc::stream_source> lexer{bfc::stream_source{new std::stringstream{""}}};
   bfc::token tok{};
   bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::DONE) << "next() should return DONE when called on empty file";
+  EXPECT_EQ(result, bfc::result_type::DONE) << "next() should return DONE when called on empty file";
 }
 
 TEST(LexerTest, OneOfEach)
 {
-  bfc::lexer<bfc::stream_source> lexer{bfc::stream_source{new std::fstream{ONE_OF_EACH_FILE_NAME}}};
+  bfc::lexer<bfc::stream_source> lexer{bfc::stream_source{new std::stringstream{"+-<>[].,"}}};
   bfc::token tok{};
   bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::INC) << "lexer should make token kind INC when reading \'+\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::DEC) << "lexer should make token kind DEC when reading \'-\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::MOVE_L) << "lexer should make token kind INC when reading \'<\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::MOVE_R) << "lexer should make token kind INC when reading \'>\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::LOOP_BEGIN) << "lexer should make token kind INC when reading \'[\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::LOOP_END) << "lexer should make token kind INC when reading \']\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::PUT_CHAR) << "lexer should make token kind INC when reading \'.\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::OK) << "next() should return OK when reading tokens";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::OK) << "next() should return OK when reading tokens";
   EXPECT_EQ(tok.kind, bfc::token::GET_CHAR) << "lexer should make token kind INC when reading \',\'";
 
-  bfc::result_type result = lexer.next(tok);
-  EXPECT_EQ(result, result_type::DONE) << "next() should return DONE when reading EOF";
+  result = lexer.next(tok);
+  EXPECT_EQ(result, bfc::result_type::DONE) << "next() should return DONE when reading EOF";
 }
 
 
