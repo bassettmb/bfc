fce9f7d30702813fc43be854566f99fdc291ac01
Connor <connor.r.mcgowan@vanderbilt.edu>
2015-12-08 01:44:09 -0600

    Added extra tests for combine set visitor

--- test/optimizer_test.cpp
+++ test/optimizer_test.cpp
@@ -56,6 +56,24 @@ TEST(OptimizerTest, CombineSetVisitor)
   auto it = ((bfc::ast_program *) node.get())->begin();
   EXPECT_NE(dynamic_cast<bfc::ast_set*>(it->get()), nullptr) << "Code should be parsed into ast_set";
   EXPECT_EQ(((bfc::ast_set*)it->get())->value(), (bf_value) 2) << "ast_set(0) and 2 INCs should be parsed into ast_set(2)";
+  
+  bfc::parser<bfc::stream_source> parser{
+    bfc::lexer<bfc::stream_source>{bfc::stream_source{new std::stringstream{"++[-]"}}}};
+  bfc::optimizer optimizer{false, true};
+  bfc::ast_node node = optimizer.optimize(parser.parse());
+  EXPECT_FALSE(((bfc::ast_program *) node.get())->empty()) << "Should not produce an empty program node";
+  auto it = ((bfc::ast_program *) node.get())->begin();
+  EXPECT_NE(dynamic_cast<bfc::ast_set*>(it->get()), nullptr) << "Code should be parsed into ast_set";
+  EXPECT_EQ(((bfc::ast_set*)it->get())->value(), (bf_value) 0) << "2 INCS and ast_set(0) should be parsed into ast_set(0)";
+  
+  bfc::parser<bfc::stream_source> parser{
+    bfc::lexer<bfc::stream_source>{bfc::stream_source{new std::stringstream{"[-]++[-]"}}}};
+  bfc::optimizer optimizer{false, true};
+  bfc::ast_node node = optimizer.optimize(parser.parse());
+  EXPECT_FALSE(((bfc::ast_program *) node.get())->empty()) << "Should not produce an empty program node";
+  auto it = ((bfc::ast_program *) node.get())->begin();
+  EXPECT_NE(dynamic_cast<bfc::ast_set*>(it->get()), nullptr) << "Code should be parsed into ast_set";
+  EXPECT_EQ(((bfc::ast_set*)it->get())->value(), (bf_value) 0) << "ast_set(0), 2 INCs, and ast_set(0) should be parsed into ast_set(2), ast_set(0), then parsed into ast_set(0)";
 }
 
 
