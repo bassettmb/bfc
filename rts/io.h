617c86a8a7abd5822cb9194e2eed3aea77f5b332
Michael Bassett <michael.b.bassett@gmail.com>
2015-12-07 21:46:22 -0600

    Add declaration for bfc_io_deinit & reorder function listing.

--- rts/io.h
+++ rts/io.h
@@ -7,8 +7,9 @@
 
 BFC_RTS_EXPORT FILE *bfc_in;
 BFC_RTS_EXPORT FILE *bfc_out;
-void bfc_io_init(FILE *input, FILE *output);
 BFC_RTS_EXPORT void bfc_putc(ptrdiff_t offset);
 BFC_RTS_EXPORT void bfc_getc(ptrdiff_t offset);
+void bfc_io_init(FILE *input, FILE *output);
+void bfc_io_deinit(int close_input, int close_output);
 
 #endif /* !BFC_RTS_IO_H */
