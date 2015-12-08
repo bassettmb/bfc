d6e9d9347a701e560da5024715cec6bb7dbfc393
Michael Bassett <michael.b.bassett@gmail.com>
2015-12-07 23:23:13 -0600

    Rename arith to primops and add bfc_set primitive op.

--- rts/primops.h
+++ rts/primops.h
@@ -8,5 +8,6 @@
 #define bfc_mov(off) (bfc_check_access(off), bfc_hp += (off))
 #define bfc_mul(off, val) (bfc_check_access(off), \
                            bfc_hp[(off)] += *bfc_hp * (val), *bfc_hp = 0)
+#define bfc_set(off, val) (bfc_check_access(off), bfc_hp[(off)] = (val))
 
 #endif /* !BFC_RTS_ARITH_H */
