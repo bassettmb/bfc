162debf3c29ccf9d35f630a64206a99b8093bba0
Michael Bassett <michael.b.bassett@gmail.com>
2015-12-07 21:45:24 -0600

    Add definition for bfc_deinit.

--- rts/init.c
+++ rts/init.c
@@ -7,3 +7,10 @@ bfc_init(FILE *input, FILE *output)
   bfc_heap_init();
   bfc_io_init(input, output);
 }
+
+void
+bfc_deinit(int close_input, int close_output)
+{
+  bfc_heap_deinit();
+  bfc_io_deinit(close_input, close_output);
+}
